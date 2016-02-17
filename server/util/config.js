32ec319809d75858352caba18451bd8aa208daf6
Ethan Rubio <ethanrubio@gmail.com>
2016-02-16 18:41:16 -0800

    only for heroku

---
+++ server/util/config.js
@@ -0,0 +1,29 @@
+// api keys
+// Yelp
+/*API v2.0
+Consumer Key	95eZS34IPxiGl7KNoJlj_A
+Consumer Secret	okgs2TQMa5mlXIFmJvRfDpw0t5U
+Token	5Hts1xcNoTn3Ih3DBNdsmhFnx2PBIbMK
+Token Secret	RAuP62I2yM8c5mz1k2aNH93zYjE*/
+
+var keys = {
+}
+
+keys.yelp = {
+  consumer_key: '95eZS34IPxiGl7KNoJlj_A',
+  consumer_secret: 'okgs2TQMa5mlXIFmJvRfDpw0t5U',
+  token: '5Hts1xcNoTn3Ih3DBNdsmhFnx2PBIbMK',
+  token_secret: 'RAuP62I2yM8c5mz1k2aNH93zYjE'
+}
+
+// Uber
+// client id Iqbl0v8Fm-Y_0Cb9HUXa50FN0KVzibfX
+// server token TPezeslMXFaQGch-iSAHB-noppMCqVqdqQK6jTXq
+
+keys.uber = {
+  client_id: 'Iqbl0v8Fm-Y_0Cb9HUXa50FN0KVzibfX',
+  client_secret: 'e4y0Gm42F49--b2nQphVPBNHJu73dOXmX66JTDDp',
+  server_token: 'TPezeslMXFaQGch-iSAHB-noppMCqVqdqQK6jTXq'
+}
+
+module.exports = keys;
