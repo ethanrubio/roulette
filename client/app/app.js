1462a697a941e18e8944eb1ef649a9eabace732e
Ethan Rubio <ethanrubio@gmail.com>
2016-04-22 19:26:32 -0700

    [chore] cleans up white space

--- client/app/app.js
+++ client/app/app.js
@@ -23,40 +23,39 @@ angular.module('pubroulette', [
 
   $scope.success = function() {
     YelpAPI.clickedFirst();
-      // get geolocation of user
-      if (navigator.geolocation) {
-         navigator.geolocation.getCurrentPosition(function(success, error) {
-           $scope.$apply(function() {
-             $scope.position = success;
-             YelpAPI.passLocation(
-                $scope.position.coords.latitude, 
-                $scope.position.coords.longitude
-               )
-              .then(function(resp) {
-                  $scope.name = resp.data.name;
-                  $scope.destination = resp.data.location.coordinate;
-                  UberAPI.passLocation(
-                    $scope.position.coords.latitude, 
-                    $scope.position.coords.longitude, 
-                    resp.data.location.coordinate.latitude, 
-                    resp.data.location.coordinate.longitude
-                  )
-                  .then(function(resp) {
-                            Map.mapStore(
-                              $scope.position.coords.latitude, 
-                              $scope.position.coords.longitude, 
-                              $scope.destination.latitude, 
-                              $scope.destination.longitude, 
-                              $scope.name
-                            );
-                          })
-              })
-          });
-          if (error) {
-            console.log(error);
-          }
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(success, error) {
+          $scope.$apply(function() {
+            $scope.position = success;
+            YelpAPI.passLocation(
+              $scope.position.coords.latitude, 
+              $scope.position.coords.longitude
+              )
+            .then(function(resp) {
+                $scope.name = resp.data.name;
+                $scope.destination = resp.data.location.coordinate;
+                UberAPI.passLocation(
+                  $scope.position.coords.latitude, 
+                  $scope.position.coords.longitude, 
+                  resp.data.location.coordinate.latitude, 
+                  resp.data.location.coordinate.longitude
+                )
+                .then(function(resp) {
+                          Map.mapStore(
+                            $scope.position.coords.latitude, 
+                            $scope.position.coords.longitude, 
+                            $scope.destination.latitude, 
+                            $scope.destination.longitude, 
+                            $scope.name
+                          );
+                        })
+            })
         });
-      }
+        if (error) {
+          console.log(error);
+        }
+      });
+    }
   };
 
 });
