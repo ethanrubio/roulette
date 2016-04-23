9aaf0cfd611b99dc94dcc66e9fde7264a84807cb
MKS Student <student@makersquare.com>
2016-04-23 10:45:00 -0700

    [feature] adds CORS override

--- server/server.js
+++ server/server.js
@@ -1,6 +1,16 @@
 var express = require('express');
-
 var app = express();
+var methodOverride = require('method-override');
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+};
+
+app.use(allowCrossDomain);
+app.use(methodOverride());
 
 require('./util/request-helper.js')(app, express);
 
