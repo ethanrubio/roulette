47f1cdf50e487fd217fa3410a41eadad062bf644
Ethan Rubio <ethanrubio@gmail.com>
2016-04-22 19:21:50 -0700

    [chore] cleans up white space and console logs

--- client/app/services/uberapifactory.js
+++ client/app/services/uberapifactory.js
@@ -1,19 +1,16 @@
 angular.module('pubroulette.uberapi', [])
-
-.factory("UberAPI", function ($http) {
+.factory("UberAPI", function($http) {
 
   var uberData = {};
 
-  var passLocation = function(startLat, startLong, endLat, endLong){
+  var passLocation = function(startLat, startLong, endLat, endLong) {
     return $http({
       method: 'POST',
       url: '/api/uber/journey',
       data: {startLat: startLat, startLong: startLong, endLat: endLat, endLong: endLong}
     })
-    .then(function(resp){
-      console.log("uber API got a response! ", resp);
+    .then(function(resp) {
       uberData.data = resp.data;
-      console.log('Hi this is the uberData in UberAPI being set', uberData);
       return resp;
     });
   };
@@ -22,4 +19,5 @@ angular.module('pubroulette.uberapi', [])
     passLocation: passLocation,
     uberData: uberData
   };
+  
 });
