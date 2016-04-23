8b290f868b71c5c75be1d980c6e6962a476b0c6b
MKS Student <student@makersquare.com>
2016-04-23 15:23:01 -0700

    [feature] adds uber request coordinates to url

--- client/app/services/uberapifactory.js
+++ client/app/services/uberapifactory.js
@@ -4,25 +4,37 @@ angular.module('pubroulette.uberapi', [])
   $http.defaults.useXDomain = true;
 
   var uberData = {};
+  var coordinates = {};
   var uberAuthenticate;
 
   var passLocation = function(startLat, startLong, endLat, endLong) {
+
+    coordinates.start_longitude = startLong;
+    coordinates.start_latitude = startLat;
+    coordinates.end_longitude = endLong;
+    coordinates.end_latitude = endLat;
+
     return $http({
       method: 'POST',
       url: '/api/uber/journey',
       data: {startLat: startLat, startLong: startLong, endLat: endLat, endLong: endLong}
     })
     .then(function(resp) {
+      console.log(resp.data);
       uberData.data = resp.data;
       return resp;
     });
   };
 
   var authenticate = function() {
+    var uberX = uberData.data.prices[0].product_id;
 
-    $window.open('http://localhost:3000/api/uber/authenticate', '_blank');
-
-  }
+    $window.open('http://localhost:3000/api/uber/authenticate?product_id=' + uberX
+      + '&start_longitude=' + coordinates.start_longitude
+      + '&start_latitude=' + coordinates.start_latitude
+      + '&end_longitude=' + coordinates.end_longitude
+      + '&end_latitude=' + coordinates.end_latitude, '_blank');
+  };
 
   return {
     passLocation: passLocation,
