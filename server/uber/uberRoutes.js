df23ff5d4f589fd51937519c74895af763c8ee53
MKS Student <student@makersquare.com>
2016-04-23 10:11:22 -0700

    [feature] adds route to uber authorize

--- server/uber/uberRoutes.js
+++ server/uber/uberRoutes.js
@@ -3,4 +3,5 @@ var uberController = require('./uberController.js');
 module.exports = function(app) {
 
   app.post('/journey', uberController.estimate);
+  app.get('/authenticate', uberController.authenticate);
 };
