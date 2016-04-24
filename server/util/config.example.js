6490519ee6210864c322dc57c99eddc8256c9e0b
Ethan Rubio <ethanrubio@gmail.com>
2016-04-24 12:29:52 -0700

    [chore] adds db example

--- server/util/config.example.js
+++ server/util/config.example.js
@@ -1,13 +1,18 @@
 // api keys
 exports.uber = {
-  client_id: "",
-  client_secret: "",
-  server_token: ""
+  client_id: '',
+  client_secret: '',
+  server_token: ''
 };
 
 exports.yelp = {
-  consumer_key: "",
-  consumer_secret: "",
-  token: "",
-  token_secret: ""
+  consumer_key: '',
+  consumer_secret: '',
+  token: '',
+  token_secret: ''
 };
+
+exports.db = {
+  username: '',
+  password: ''
+}
