518e4f082e4f6a1d1d8a771df85fb763425613c1
Ethan Rubio <ethanrubio@gmail.com>
2016-04-24 15:52:36 -0700

    [feature] adds login pop up with satellizer

--- client/app/app.js
+++ client/app/app.js
@@ -7,20 +7,28 @@ angular.module('pubroulette', [
   'pubroulette.mapMaker',
   'pubroulette.loader',
   'pubroulette.roulette',
-  'ui.router'
+  'pubroulette.auth',
+  'ui.router',
+  'satellizer'
 ])
-.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
-
+.config(['$stateProvider', '$urlRouterProvider', '$authProvider', function($stateProvider, $urlRouterProvider, $authProvider) {
+  
+  $authProvider.oauth2({
+    name: 'uber',
+    url: '/api/uber/token',
+    redirectUri: window.location.origin,
+    authorizationEndpoint: 'http://localhost:3468/api/uber/authenticate',
+    popupOptions: { width: 500, height: 560 }
+  });
 
   $stateProvider
     .state('homepage', {
       url: '/',
       controller: 'AppController'
-    })
+    });
 
     $urlRouterProvider.otherwise('/');
 
 }])
 .controller('AppController', ['$scope', function($scope) {
-
 }]);
