2a839d89ed47a55ac3bca47ece7e7126f761df75
Ethan Rubio <ethanrubio@gmail.com>
2016-04-24 13:27:38 -0700

    [chore] adds connection to mongodb with monogoose

--- server/server.js
+++ server/server.js
@@ -1,5 +1,12 @@
 var express = require('express');
 var app = express();
+var db = require('./util/config').db;
+var mongoose = require('mongoose');
+
+var mongooseUsername = db.username;
+var mongoosePassword = db.password;
+
+mongoose.connect('mongodb://'+mongooseUsername+':'+mongoosePassword+'@ds032319.mlab.com:32319/pubroulette');
 
 require('./util/request-helper.js')(app, express);
 
