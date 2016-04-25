377fbbb6a0fabca0bfe4a9595a447301405bf6e1
Ethan Rubio <ethanrubio@gmail.com>
2016-04-24 19:06:14 -0700

    [chore] cleans up white space

--- client/app/roulette/roulette.js
+++ client/app/roulette/roulette.js
@@ -21,23 +21,23 @@ angular.module('pubroulette.roulette', [])
               $scope.position.coords.longitude
               )
             .then(function(resp) {
-                $scope.name = resp.data.name;
-                $scope.destination = resp.data.location.coordinate;
-                UberAPI.passLocation(
-                  $scope.position.coords.latitude, 
-                  $scope.position.coords.longitude, 
-                  resp.data.location.coordinate.latitude, 
-                  resp.data.location.coordinate.longitude
+              $scope.name = resp.data.name;
+              $scope.destination = resp.data.location.coordinate;
+              UberAPI.passLocation(
+                $scope.position.coords.latitude, 
+                $scope.position.coords.longitude, 
+                resp.data.location.coordinate.latitude, 
+                resp.data.location.coordinate.longitude
                 )
                 .then(function(resp) {
-                          Map.mapStore(
-                            $scope.position.coords.latitude, 
-                            $scope.position.coords.longitude, 
-                            $scope.destination.latitude, 
-                            $scope.destination.longitude, 
-                            $scope.name
-                          );
-                        })
+                  Map.mapStore(
+                    $scope.position.coords.latitude, 
+                    $scope.position.coords.longitude, 
+                    $scope.destination.latitude, 
+                    $scope.destination.longitude, 
+                    $scope.name
+                    );
+                })
             })
         });
         if (error) {
