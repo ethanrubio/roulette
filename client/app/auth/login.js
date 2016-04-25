03ecefcd490a4e150681e68c20a14bf552c117d4
Ethan Rubio <ethanrubio@gmail.com>
2016-04-24 20:29:37 -0700

    [feature] adds personal directive

--- client/app/auth/login.js
+++ client/app/auth/login.js
@@ -1,10 +1,12 @@
 angular.module('pubroulette.login', [])
 
-.controller('LoginController', ['$scope', '$auth', function($scope, $auth) {
+.controller('LoginController', ['$scope', '$auth', 'jwtHelper', function($scope, $auth, jwtHelper) {
   
-  var loginData = {};
+  var vm = this;
+  
+  vm.loginData = {};
     
-  $scope.checkAuth = function() {
+  vm.checkAuth = function() {
     var result = false;
     if ($auth.isAuthenticated()) {
       result = true;
@@ -12,10 +14,11 @@ angular.module('pubroulette.login', [])
     return result;
   };
   
-  $scope.authenticate = function(provider) {
+  vm.authenticate = function(provider) {
     $auth.authenticate(provider)
-      .then(function(data) {
-        loginData = data;
+      .then(function(resp) {
+        var tokenPayload = jwtHelper.decodeToken(resp.data.token);
+        vm.loginData = tokenPayload;
       })
       .catch(function(error) {
         console.error('there has been an error! ', error);
@@ -26,12 +29,18 @@ angular.module('pubroulette.login', [])
 .directive('ngUberAuth', function() {
   return {
     restrict: 'E',
-    template: "<md-button ng-if='checkAuth() === false;' class='md-raised md-primary' style='margin: 5em; width: 15em; background-color: #01579b;' ng-click='authenticate(\"uber\")'><i class='icon ion-model-s'></i> Sign in with Uber</md-button>"
+    template: "<md-button ng-if='vm.checkAuth() === false;' class='md-raised md-primary' style='margin: 5em; width: 15em; background-color: #01579b;' ng-click='vm.authenticate(\"uber\")'><i class='icon ion-model-s'></i> Sign in with Uber</md-button>",
+    controller: 'LoginController',
+    controllerAs: 'vm',
+    bindToController: true
   };
 })
-.directive('ngUberPersonal', function() {
+.directive('ngPersonal', function() {
   return {
     restrict: 'E',
-    template: '<img ng-src="{{loginData.picture}}" class="md-avatar" alt="{{loginData.first_name}}" /> <div class="md-list-item-text compact"> <h3>{{loginData.first_name}}</h3> </div>'
+    template: '<md-list class="fixedRows" ng-if="vm.checkAuth() === true;"><md-list-item class="md-2-line contact-item"><img ng-src="{{vm.loginData.picture}}" class="md-avatar" alt="{{vm.loginData.first_name}}" /> <div class="md-list-item-text compact"> <h3 style="color: inherit">{{vm.loginData.first_name}}</h3> </div></md-list-item></md-list>',
+    controller: 'LoginController',
+    controllerAs: 'vm',
+    bindToController: true 
   }; 
 })
