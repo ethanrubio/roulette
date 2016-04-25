43697d92a7e2fafc70ca4b2b03a532e6dd7c5ec1
Ethan Rubio <ethanrubio@gmail.com>
2016-04-24 19:41:10 -0700

    [feature] adds uber personal directive

--- client/app/auth/login.js
+++ client/app/auth/login.js
@@ -1,6 +1,8 @@
 angular.module('pubroulette.login', [])
 
 .controller('LoginController', ['$scope', '$auth', function($scope, $auth) {
+  
+  var loginData = {};
     
   $scope.checkAuth = function() {
     var result = false;
@@ -13,7 +15,7 @@ angular.module('pubroulette.login', [])
   $scope.authenticate = function(provider) {
     $auth.authenticate(provider)
       .then(function(data) {
-
+        loginData = data;
       })
       .catch(function(error) {
         console.error('there has been an error! ', error);
@@ -24,6 +26,12 @@ angular.module('pubroulette.login', [])
 .directive('ngUberAuth', function() {
   return {
     restrict: 'E',
-    template: "<md-button class='md-raised md-primary' style='margin: 5em; width: 15em; background-color: #01579b;' ng-click='authenticate(\"uber\")'><i class='icon ion-model-s'></i> Sign in with Uber</md-button>"
+    template: "<md-button ng-if='checkAuth() === false;' class='md-raised md-primary' style='margin: 5em; width: 15em; background-color: #01579b;' ng-click='authenticate(\"uber\")'><i class='icon ion-model-s'></i> Sign in with Uber</md-button>"
   };
+})
+.directive('ngUberPersonal', function() {
+  return {
+    restrict: 'E',
+    template: '<img ng-src="{{loginData.picture}}" class="md-avatar" alt="{{loginData.first_name}}" /> <div class="md-list-item-text compact"> <h3>{{loginData.first_name}}</h3> </div>'
+  }; 
 })
