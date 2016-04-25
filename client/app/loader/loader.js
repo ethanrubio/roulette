0f3535011a03fa86f9da062eb469cb827940fc14
Ethan Rubio <ethanrubio@gmail.com>
2016-04-24 22:22:58 -0700

    [refactor] changes loader logic to better handle repeat showing

--- client/app/loader/loader.js
+++ client/app/loader/loader.js
@@ -1,18 +1,10 @@
 angular.module('pubroulette.loader', [])
 
-.controller('LoaderController', ['$scope', 'Map', 'YelpAPI', function($scope, Map, YelpAPI) {
-
-  $scope.mapShow = function() {
+.controller('LoaderController', ['$scope', 'Map', 'YelpAPI', 'Loader', function($scope, Map, YelpAPI, Loader) {
+  
+  $scope.show = function() {
     var result = false;
-    if (Map.mapShow() === true) {
-      result = true;
-    }
-    return result;
-  };
-
-  $scope.clicked = function() {
-    var result = false;
-    if (YelpAPI.clicked() === true) {
+    if (Loader.loaderShow() === true) {
       result = true;
     }
     return result;
@@ -22,6 +14,6 @@ angular.module('pubroulette.loader', [])
 .directive('ngLoading', function() {
   return {
     restrict: 'E',
-    template: "<md-progress-linear ng-if='mapShow() === false && clicked() === true' md-mode='indeterminate'></md-progress-linear>"
+    template: "<md-progress-linear ng-if='show() === true' md-mode='indeterminate'></md-progress-linear>"
   };
 })
